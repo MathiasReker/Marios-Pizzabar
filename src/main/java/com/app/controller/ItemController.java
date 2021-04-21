720e3008d323ebfe17c1020a672164411d2cc8ac
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-04-21 16:25:03 +0200

    Adds removeItem and utility methods

--- src/main/java/com/app/controller/ItemController.java
+++ src/main/java/com/app/controller/ItemController.java
@@ -7,12 +7,13 @@ import com.app.view.ItemView;
 import java.util.Scanner;
 
 public class ItemController {
-  private final ItemParser ITEM_LOADER = new ItemParser("data/itemdb/items.txt"); //swap to Path from config
+  private final ItemParser ITEM_PARSER =
+      new ItemParser("data/itemdb/items2.txt"); //TODO swap to Path from config
   private final ItemView ITEM_VIEW = new ItemView();
-  private ItemModel[] itemModels = ITEM_LOADER.getItemsFromFile();
+  private ItemModel[] itemModels = ITEM_PARSER.getItemsFromFile();
   private Scanner scanner = new Scanner(System.in);
 
-  public void createItem(){
+  public void createItem() {
     ITEM_VIEW.printTxt("Input ID");
     String id = scanner.nextLine();
 
@@ -23,10 +24,49 @@ public class ItemController {
     String itemDescription = scanner.nextLine();
 
     ITEM_VIEW.printTxt("Input price");
-    int price = scanner.nextInt(); //TODO Add validation
+    int price = scanner.nextInt(); // TODO Add validation
 
-    ItemModel newItem = new ItemModel(id,itemName,itemDescription, price);
-        
-    //ITEM_LOADER.saveItemsToFile();
+    ItemModel newItem = new ItemModel(id, itemName, itemDescription, price);
+
+    itemModels = appendItem(newItem);
+    ITEM_PARSER.saveItemsToFile(itemModels);
+  }
+
+
+  public void deleteItem(){
+    ITEM_VIEW.printTxt("Item to delete");
+    int input = scanner.nextInt();
+
+    itemModels = removeElement(input);
+
+    ITEM_PARSER.saveItemsToFile(itemModels);
+  }
+
+
+  private ItemModel[] appendItem(ItemModel item){ //TODO Consider just using ArrayList
+    ItemModel[] result = new ItemModel[itemModels.length+1];
+    for(int i = 0; i < itemModels.length; i++){
+      result[i] = itemModels[i];
+    }
+    result[itemModels.length] = item;
+
+    return result;
+  }
+
+  private ItemModel[] removeElement(int index){ //TODO Consider just using ArrayList
+    ItemModel[] result = new ItemModel[itemModels.length-1];
+    int counter = 0;
+    for(int i = 0; i < itemModels.length; i++){
+      if (index != i){
+        result[counter] = itemModels[i];
+        counter++;
+      }
+    }
+    return result;
+  }
+
+
+  public ItemModel[] getItemModels() {
+    return itemModels;
   }
 }
