6790fe8b849596813073eecfbc2dafd5dad51a80
Mathias Reker <mathias@reker.dk>
2021-04-21 18:32:58 +0200

    Update tests

--- src/test/java/com/app/model/ConfigParserModelTest.java
+++ src/test/java/com/app/model/ConfigParserModelTest.java
@@ -3,16 +3,16 @@ package com.app.model;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class ConfigParserModelTest {
+public class ConfigParserModelTest {
   @Test
-  void testGetInvalidPath() {
+  public void testGetInvalidPath() {
     ConfigParserModel configParserModel = new ConfigParserModel("Test");
     String result = configParserModel.getPath();
     Assertions.assertEquals("", result);
   }
 
   @Test
-  void testGetValidPath() {
+  public void testGetValidPath() {
     ConfigParserModel configParserModel = new ConfigParserModel("itemDb");
     String result = configParserModel.getPath();
     Assertions.assertEquals("data/itemdb/items.txt", result);
