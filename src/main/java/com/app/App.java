3cd119ebeee423f39f68777bac3afef23e758a49
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-04-21 13:52:14 +0200

    reverted changes in app.java

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -6,11 +6,12 @@ import com.app.model.menuActions.*;
 
 public class App {
   public static void main(String[] args) {
+    createOrderMenuAction test2 = new createOrderMenuAction();
     exitMenuAction test3 = new exitMenuAction();
-    MenuAction[] menu = {test3};
+    MenuAction[] menu = {test2,test3};
     MenuController test = new MenuController("Hello", "Hello", menu);
 
 
-    //test.run();
+    test.run();
   }
 }
