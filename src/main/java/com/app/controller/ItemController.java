0e2855882f908893f23f3714511201c1cefcc7bc
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-04-21 16:02:57 +0200

    initial createItem method

--- src/main/java/com/app/controller/ItemController.java
+++ src/main/java/com/app/controller/ItemController.java
@@ -1,4 +1,32 @@
 package com.app.controller;
 
+import com.app.model.ItemModel;
+import com.app.model.ItemParser;
+import com.app.view.ItemView;
+
+import java.util.Scanner;
+
 public class ItemController {
+  private final ItemParser ITEM_LOADER = new ItemParser("data/itemdb/items.txt"); //swap to Path from config
+  private final ItemView ITEM_VIEW = new ItemView();
+  private ItemModel[] itemModels = ITEM_LOADER.getItemsFromFile();
+  private Scanner scanner = new Scanner(System.in);
+
+  public void createItem(){
+    ITEM_VIEW.printTxt("Input ID");
+    String id = scanner.nextLine();
+
+    ITEM_VIEW.printTxt("Input Item Name");
+    String itemName = scanner.nextLine();
+
+    ITEM_VIEW.printTxt("Input Item description");
+    String itemDescription = scanner.nextLine();
+
+    ITEM_VIEW.printTxt("Input price");
+    int price = scanner.nextInt(); //TODO Add validation
+
+    ItemModel newItem = new ItemModel(id,itemName,itemDescription, price);
+        
+    //ITEM_LOADER.saveItemsToFile();
+  }
 }
