de932d007b9c17cd85e7eb071083573f338bb819
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-04-21 16:40:44 +0200

    created OrderParserModel class

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -16,5 +16,6 @@ public class App {
 
     new MenuController("Main Menu", "Please input number: ", menu).run();
 
+
   }
 }
--- src/main/java/com/app/model/OrderParserModel.java
+++ src/main/java/com/app/model/OrderParserModel.java
@@ -1,17 +1,65 @@
-package java.com.app.model;
+package com.app.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class OrderParserModel {
+  private final String PATH;
+  private final FileHandler ORDERFILE;
+  private final String FILENAME = LocalDate.now().toString();
 
-/*  public String orderToString(OrderModel [] order){//TODO needs to be refactored
 
-    String orderToString = "";
+  public OrderParserModel(String path) {
+    PATH = path + FILENAME;
+    ORDERFILE = new FileHandler(PATH);
+  }
 
-    for (int i = 0; i < orderLines.size(); i++) {
-      orderToString +=  orderLines.get(i).orderLineToString();
+  public OrderModel[] getOrdersFromFrile(){
+
+    ArrayList<String> orderString = ORDERFILE.readFile();
+    OrderModel [] result = new OrderModel[orderString.size()];
+    ArrayList<OrderLineModel> orderLines;
+
+    for (int i = 0; i <orderString.size() ; i++) {
+      String[] splitValues = orderString.get(i).split(";");
+
+      orderLines = stringToArrayList(splitValues[1]);
+
+      result[i] =
+          new OrderModel(LocalDateTime.parse(splitValues[0]), orderLines,splitValues[2],
+              LocalDateTime.parse(splitValues[3]), Integer.parseInt(splitValues[4]));
+
     }
+    return result;
+  }
 
-    orderToString +=";" + orderId + ";" + expectedPickUpTime + ";" + orderStatus;
+  public ArrayList<OrderLineModel> stringToArrayList(String s){
+    String temp = s;
+    ArrayList<OrderLineModel> orderLines = new ArrayList<>();
 
-    return orderToString;
-  }*/
+    for (int j = 0; j < 2; j++) {
+      String[] splitValues2 = temp.split("@");
+      orderLines.add( new OrderLineModel(Integer.parseInt(splitValues2[0]), splitValues2[1], Integer.parseInt(splitValues2[2])));
+    }
+    return orderLines;
+  }
+
+  public void saveOrdersToFile(OrderModel[] orders) {
+
+    String[] result = new String[orders.length];
+
+    for (int i = 0; i < result.length; i++) {
+      result[i] =
+          String.join(
+              ";",
+              orders[i].getTimeOfOrder().toString(),
+              orders[i].getOrderLines().get(i).getFormattedOrderLine(),
+              orders[i].getOrderId(),
+              orders[i].getExpectedPickUpTime().toString(),
+              String.valueOf(orders[i].getOrderStatus()));
+    }
+    ORDERFILE.writeFile(result);
+  }
+
 }
