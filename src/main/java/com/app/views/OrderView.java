8af8c884a0024ffc3fae880650223c6c656cf1f3
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-04-22 11:47:32 +0200

    updated viewOrders and OrderView.java

--- src/main/java/com/app/views/OrderView.java
+++ src/main/java/com/app/views/OrderView.java
@@ -1,46 +1,35 @@
 package com.app.views;
 
-import com.app.models.OrderModel;
-
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 
 public class OrderView {
 
-  public void printReceipt(OrderModel order) { //TODO Refactor to controller
-
-    DateTimeFormatter dtmf = DateTimeFormatter.ofPattern("dd-MM-yyy hh:mm");
-    LocalDateTime timeOfOrder = order.getTimeOfOrder();
-
-    System.out.println("******Order Number: " + order.getOrderId() + " ******");
-    System.out.println();
-    System.out.println("Order Recieved: " + timeOfOrder);
-    for (int i = 0; i < order.getOrderLines().size(); i++) {
-      System.out.println(
-          (i + 1) + ". \t" +
-              "Pizza number: " + order.getOrderLines().get(i).getItem() + "\t" +
-              "Quantity: " + order.getOrderLines().get(i).getQty() + "\t" +
-              "Price: " + order.getOrderLines().get(i).getUnitPrice() + "\t" +
-              "Sub total: " + order.getOrderLines().get(i).getSubTotal()
-      );
+  public void printReceipt(
+      String orderID, LocalDateTime timeOfOrder, String[] orderLines, int totalPrice) {
+    System.out.printf("******Order Number: %s ****** \n", orderID);
+    System.out.printf("Order received: %s \n", timeOfOrder);
+    for (int i = 0; i < orderLines.length; i++) {
+      String[] orderLineData = orderLines[i].split(";");
+      System.out.printf("Pizza number: %s\t", orderLineData[0]);
+      System.out.printf("Pizza name: %s\t", orderLineData[1]);
+      System.out.printf("Price: %s\t", formatPrice(orderLineData[3]));
+      System.out.printf("Quantity: %s\t", orderLineData[2]);
+      System.out.printf("Subtotal: %s\t", orderLineData[4]);
+      System.out.println();
     }
-    System.out.println("Total price: " + order.totalPrice());
+    System.out.printf("Total price: %s", totalPrice);
     System.out.println();
   }
 
-  public void printReceipt(String orderID, LocalDateTime timeOfOrder, String[] orderLines, int totalPrice){
-    System.out.printf("******Order Number: %s ******",orderID);
-    System.out.println();
-    System.out.printf("Order received: %s",timeOfOrder);
-    for(String s : orderLines){
-      System.out.println(s);
-    }
-    System.out.printf("Total price: ",totalPrice);
-    System.out.println();
-  }
-
-  public void printTxt(String message){
+  public void printTxt(String message) {
     System.out.print(message);
+  }
+
+  private String formatPrice(String price) {
+    String wholeNumber = String.valueOf(price);
+    String decimals = wholeNumber.substring(wholeNumber.length() - 2);
+    wholeNumber = wholeNumber.substring(0, decimals.length());
+
+    return wholeNumber + "," + decimals;
   }
 }
