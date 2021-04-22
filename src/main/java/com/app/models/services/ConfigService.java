f6fe1baed958c3c199a17dff6290e2541d1d147f
Mathias Reker <mathias@reker.dk>
2021-04-22 11:35:00 +0200

    Refactor + tests

--- src/main/java/com/app/models/services/ConfigService.java
+++ src/main/java/com/app/models/services/ConfigService.java
@@ -4,31 +4,24 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class ConfigService {
-  String key;
+  private final String KEY;
 
   public ConfigService(String key) {
-    setKey(key);
-  }
-
-  public void setKey(String key) {
-    // Validate
-    this.key = key;
+    this.KEY = key;
   }
 
   public String getPath() throws FileNotFoundException {
     FileService file = new FileService("data/config.txt");
     ArrayList<String> fileInfo = file.readFile();
 
-    String result = "";
-
     for (String s : fileInfo) {
       String[] arr = s.split(";");
 
-      if (arr[0].equals(key)) {
-        result = arr[1];
+      if (arr[0].equals(KEY)) {
+        return arr[1];
       }
     }
 
-    return result;
+    return null;
   }
 }
