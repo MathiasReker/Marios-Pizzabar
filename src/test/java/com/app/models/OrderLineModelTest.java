e8862fa9906c35b3a82773a8a02947e80467e709
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-04-25 14:02:01 +0200

    OrderLineModelTest class changed to public

--- src/test/java/com/app/models/OrderLineModelTest.java
+++ src/test/java/com/app/models/OrderLineModelTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class OrderLineModelTest {
+public class OrderLineModelTest {
 
   @Test
   public void testGetValidSubTotal() {
