c41c431a1d313e3596f4055633bc9b700a8b83ca
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-04-25 13:11:16 +0200

    added test to name in tests

--- src/test/java/com/app/models/OrderLineModelTest.java
+++ src/test/java/com/app/models/OrderLineModelTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderLineModelTest {
 
   @Test
-  public void getValidSubTotal() {
+  public void testGetValidSubTotal() {
 
     ItemModel itemModel = new ItemModel("1", "pizza", "tomat ost", 500);
     OrderLineModel orderLine = new OrderLineModel(3, itemModel);
@@ -19,7 +19,7 @@ class OrderLineModelTest {
   }
 
   @Test
-  public void getInValidSubTotal() {
+  public void testGetInValidSubTotal() {
 
     ItemModel itemModel = new ItemModel("1", "pizza", "tomat ost", 500);
     OrderLineModel orderLine = new OrderLineModel(3, itemModel);
@@ -31,7 +31,7 @@ class OrderLineModelTest {
   }
 
   @Test
-  public void getValidQuantity(){
+  public void testGetValidQuantity(){
 
     ItemModel itemModel = new ItemModel("1", "pizza", "tomat ost", 500);
     OrderLineModel orderLine = new OrderLineModel(3, itemModel);
@@ -45,7 +45,7 @@ class OrderLineModelTest {
   }
 
   @Test
-  public void getInValidQuantity(){
+  public void testGetInValidQuantity(){
 
     ItemModel itemModel = new ItemModel("1", "pizza", "tomat ost", 500);
     OrderLineModel orderLine = new OrderLineModel(3, itemModel);
@@ -59,23 +59,37 @@ class OrderLineModelTest {
   }
 
   @Test
-  public void setInValidQty(){
+  public void testSetInValidQty(){
     ItemModel itemModel = new ItemModel("1", "pizza", "tomat ost", 500);
     OrderLineModel orderLine = new OrderLineModel(1, itemModel);
 
     assertThrows(IllegalArgumentException.class, () -> orderLine.setQty(-3));
   }
 
-  @Test void getValidUnitPrice(){
+  @Test
+  void testGetValidUnitPrice(){
 
     ItemModel itemModel = new ItemModel("1", "pizza", "tomat ost", 500);
     OrderLineModel orderLine = new OrderLineModel(3, itemModel);
 
 
-    int expected = 3;
-    int actual = orderLine.getQty();
+    int expected = 500;
+    int actual = orderLine.getUnitPrice();
 
     assertEquals(expected, actual);
+
+  }
+  @Test
+  void testGetInValidUnitPrice(){
+
+    ItemModel itemModel = new ItemModel("1", "pizza", "tomat ost", 500);
+    OrderLineModel orderLine = new OrderLineModel(3, itemModel);
+
+
+    int expected = 400;
+    int actual = orderLine.getUnitPrice();
+
+    assertNotEquals(expected, actual);
 
   }
 
